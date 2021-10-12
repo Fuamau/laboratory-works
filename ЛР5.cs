319d142f5c1a5cb095d887321eb39b04d37874ee
Диана <91161878+Fuamau@users.noreply.github.com>
2021-10-12 16:03:44 +0300

    Update ЛР5.cs

--- ЛР5.cs
+++ ЛР5.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ЛР5
 {
@@ -15,48 +15,34 @@ namespace ЛР5
             int x2 = int.Parse(Console.ReadLine());
             Console.WriteLine("Введите y2:");
             int y2 = int.Parse(Console.ReadLine());
-            int x = Convert.ToInt32(Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)));
+            float x = Convert.ToSingle(Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)));
             Console.WriteLine($"Расстояние между двумя точками равно {x}");
 
             Console.WriteLine("Задание 2.");
-            Console.WriteLine("Точка А:");
-            Console.WriteLine("Введите x1:");
-            int x3 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите y1:");
-            int y3 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Точка В:");
-            Console.WriteLine("Введите x2:");
-            int x4 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите y2:");
-            int y4 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Точка С:");
-            Console.WriteLine("Введите x3:");
-            int x5 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите y3:");
-            int y5 = int.Parse(Console.ReadLine());
-            int y = Convert.ToInt32(Math.Sqrt(Math.Pow((x5 - x3), 2) + Math.Pow((y5 - y3), 2)));
-            int z = Convert.ToInt32(Math.Sqrt(Math.Pow((x5 - x4), 2) + Math.Pow((y5 - y4), 2)));
-            Console.WriteLine($"AC+BC = {y}+{z} = {y+z}");
+            Console.WriteLine("Введите A:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите B:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите C:");
+            int c = int.Parse(Console.ReadLine());
+            int ac = Math.Abs(c-a);
+            int bc = Math.Abs(c-b);
+            Console.WriteLine($"AC+BC = {ac}+{bc} = {ac+bc}");
 
             Console.WriteLine("Задание 3.");
-            Console.WriteLine("Точка А:");
-            Console.WriteLine("Введите x1:");
-            int x6 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите y1:");
-            int y6 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Точка В:");
-            Console.WriteLine("Введите x2:");
-            int x7 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите y2:");
-            int y7 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Точка С:");
-            Console.WriteLine("Введите x3:");
-            int x8 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите y3:");
-            int y8 = int.Parse(Console.ReadLine());
-            int q = Convert.ToInt32(Math.Sqrt(Math.Pow((x8 - x6), 2) + Math.Pow((y8 - y6), 2)));
-            int w = Convert.ToInt32(Math.Sqrt(Math.Pow((x8 - x7), 2) + Math.Pow((y8 - y7), 2)));
-            Console.WriteLine($"AC*BC = {q}*{w} = {q * w}");
+            Console.WriteLine("Введите A:");
+            int a1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите B:");
+            int b1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите C:");
+            int c1 = int.Parse(Console.ReadLine());
+            if (c1 > a1 && c1 < b1)
+            {
+                int ac1 = Math.Abs(c1-a1);
+                int bc1 = Math.Abs(c1-b1);
+                Console.WriteLine($"AC*BC = {ac1}*{bc1} = {ac1 * bc1}");
+            }
+            else Console.WriteLine("Точка C не расположена между точками A и B.");
 
             Console.WriteLine("Задание 4.");
             Console.WriteLine("Введите x1:");
@@ -87,12 +73,12 @@ namespace ЛР5
             int x13 = int.Parse(Console.ReadLine());
             Console.WriteLine("Введите y3:");
             int y13 = int.Parse(Console.ReadLine());
-            int a = Convert.ToInt32(Math.Sqrt(Math.Pow((x12 - x11), 2) + Math.Pow((y12 - y11), 2)));
-            int b = Convert.ToInt32(Math.Sqrt(Math.Pow((x13 - x12), 2) + Math.Pow((y13 - y12), 2)));
-            int c = Convert.ToInt32(Math.Sqrt(Math.Pow((x13 - x11), 2) + Math.Pow((y13 - y11), 2)));
-            int P = a + b + c;
-            int p2 = P / 2;
-            int S = Convert.ToInt32(Math.Sqrt(p2 * (p2 - a) * (p2 - b) * (p2 - c)));
+            float a2 = Convert.ToSingle(Math.Sqrt(Math.Pow((x12 - x11), 2) + Math.Pow((y12 - y11), 2)));
+            float b2 = Convert.ToSingle(Math.Sqrt(Math.Pow((x13 - x12), 2) + Math.Pow((y13 - y12), 2)));
+            float c2 = Convert.ToSingle(Math.Sqrt(Math.Pow((x13 - x11), 2) + Math.Pow((y13 - y11), 2)));
+            float P = a2 + b2 + c2;
+            float p2 = P / 2;
+            float S = Convert.ToSingle(Math.Sqrt(p2 * (p2 - a2) * (p2 - b2) * (p2 - c2)));
             Console.WriteLine($"Периметр = {P} Площадь = {S}");
         }
     }
